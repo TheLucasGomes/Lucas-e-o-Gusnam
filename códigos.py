9d398ba7cf7d7a79ecab057807f9d3e4bd9d70f1
Gustavo Nam <gustavosmn@al.insper.edu.br>
2022-11-10 13:32:03 -0300

    sistema de repetição otimizado, erros no sistema de pulos, dicas e erros arrumado

--- códigos.py
+++ códigos.py
@@ -6,6 +6,7 @@ lista1 = []
 ajudas = 3
 pulos = 2
 cont = 1
+key_word = ''
 lista_niveis = funcoes.transforma_base(funcoes.quest)
 for k, v in lista_niveis.items():
     if k not in niveis:
@@ -15,63 +16,139 @@ print(f'Olá \033[0;35m{jogador}\033[0;m, vamos começar?\n')
 while pontos < 1000000:
     if pontos == 0:
         print(f'Vamos começar com as questões de nivel {niveis[0]}')
-    if 0 <= pontos < 6000 and len(niveis) >= 1:
+    if 0 <= pontos < 1000:
         print(f'PONTUAÇÃO ATUAL: {pontos}')
         quest1 = funcoes.sorteia_questao_inedida(lista_niveis, 'facil', lista1)
         prints = funcoes.questao_para_texto(quest1, cont)
+        while 0 <= pontos < 1000:
 
-        rus = str(input(f'''para dica digite "J" ({ajudas} restantes)
-para pular questão digite "P" ({pulos} restantes)
-para parar o jogo digite "S"\n
-Digite qual opção você acha que é a certa:'''))
-        if rus == quest1['correta']:
-            pontos += 1000
-            print(rus)
-            print('Certa resposta!')
-            cont += 1
-        elif rus == 'S' or rus == 's':
-            break
-        elif rus == 'J' or rus == 'j':
-            if ajudas > 0:
-                ajudas -= 1
-                ajd = funcoes.gera_ajuda(quest1)
-                print(ajd)
-                rus2 = str(input('Digite qual opção você acha que é a certa:'))
-                if rus2 == quest1['correta']:
-                    pontos += 1000
-                    print(rus2)
-                    print('Certa resposta!')
-                    cont += 1
-                elif rus2 == 'p' or rus2 == 'P':
-                    pulos -= 1
-                    print(rus2)
-                    print('Questão pulada :( ')
+            rus = str(input(f'''para dica digite "J" ({ajudas} restantes)
+    para pular questão digite "P" ({pulos} restantes)
+    para parar o jogo digite "S"\n
+    Digite qual opção você acha que é a certa:'''))
+            if rus == quest1['correta']:
+                pontos = 1000
+                print(rus)
+                print('Certa resposta!')
+                cont += 1
+                break
+            elif rus == 'S' or rus == 's':
+                key_word = 'babana'
+                break
+            elif key_word == 'babana':
+                break
+            elif rus == 'J' or rus == 'j':
+                if ajudas > 0:
+                    ajudas -= 1
+                    ajd = funcoes.gera_ajuda(quest1)
+                    print(ajd)
                 else:
-                    print(rus2)
-                    print('Errou')
+                    print('sem ajudas restantes')
+                    print(prints)
+
+            elif rus == 'P' or rus == 'p':
+                if pulos > 0:
+                    pulos -= 1
+                    print(rus)
+                    print('Questão pulada :( ')
                     break
+                else:
+                    print('Acabaram os pulos')
+                    print(prints)
             else:
-                print('sem ajudas restantes')
-                rus2 = str(input('Digite qual opção você acha que é a certa:'))
-                if rus2 == quest1['correta']:
-                    pontos += 1000
-                    print(rus2)
+                print(rus)
+                print('Errou')
+                key_word = 'babana'
+                break
+            if key_word == 'babana':
+                break
+    if key_word == 'babana':
+        break
+
+    elif 1000 <= pontos < 5000:
+        if pontos == 1000:
+            print(f'Vamos continuar com as questões de nivel {niveis[1]}')
+        if 1000 <= pontos < 5000:
+            print(f'PONTUAÇÃO ATUAL: {pontos}')
+            quest1 = funcoes.sorteia_questao_inedida(lista_niveis, 'medio', lista1)
+            prints = funcoes.questao_para_texto(quest1, cont)
+            while 1000 <= pontos < 5000:
+
+                rus = str(input(f'''para dica digite "J" ({ajudas} restantes)
+        para pular questão digite "P" ({pulos} restantes)
+        para parar o jogo digite "S"\n
+        Digite qual opção você acha que é a certa:'''))
+                if rus == quest1['correta']:
+                    pontos = 1000
+                    print(rus)
                     print('Certa resposta!')
                     cont += 1
-                elif rus2 == 'p' or rus2 == 'P':
-                    pulos -= 1
-                    print(rus2)
-                    print('Questão pulada :( ')
-                else:
-                    print(rus2)
-                    print('Errou')
                     break
+                elif rus == 'S' or rus == 's':
+                    key_word = 'babana'
+                    break
+                elif key_word == 'babana':
+                    break
+                elif rus == 'J' or rus == 'j':
+                    if ajudas > 0:
+                        ajudas -= 1
+                        ajd = funcoes.gera_ajuda(quest1)
+                        print(ajd)
+                    else:
+                        print('sem ajudas restantes')
+                        print(prints)
 
-        elif rus == 'P' or rus == 'p':
-            pulos -= 1
-            print(rus)
-            print('Questão pulada :( ')
-        else:
-            print(rus)
-            print('Errou')
+                elif rus == 'P' or rus == 'p':
+                    if pulos > 0:
+                        pulos -= 1
+                        print(rus)
+                        print('Questão pulada :( ')
+                        break
+                    else:
+                        print('Acabaram os pulos')
+                        print(prints)
+                else:
+                    print(rus)
+                    print('Errou')
+                    key_word = 'babana'
+                    break
+                if key_word == 'babana':
+                    break
+        if key_word == 'babana':
+            break
+    if 5000 <= pontos < 10000 and len(niveis) >= 1:
+        print(f'PONTUAÇÃO ATUAL: {pontos}')
+        quest1 = funcoes.sorteia_questao_inedida(lista_niveis, 'facil', lista1)
+        prints = funcoes.questao_para_texto(quest1, cont)
+        while 0 <= pontos < 1000 and len(niveis) >= 1:
+
+            rus = str(input(f'''para dica digite "J" ({ajudas} restantes)
+            para pular questão digite "P" ({pulos} restantes)
+            para parar o jogo digite "S"\n
+            Digite qual opção você acha que é a certa:'''))
+            if rus == quest1['correta']:
+                pontos = 1000
+                print(rus)
+                print('Certa resposta!')
+                cont += 1
+                break
+            elif rus == 'S' or rus == 's':
+                break
+            elif rus == 'J' or rus == 'j':
+                if ajudas > 0:
+                    ajudas -= 1
+                    ajd = funcoes.gera_ajuda(quest1)
+                    print(ajd)
+                else:
+                    print('sem ajudas restantes')
+
+            elif rus == 'P' or rus == 'p':
+                pulos -= 1
+                print(rus)
+                print('Questão pulada :( ')
+                break
+            else:
+                print(rus)
+                print('Errou')
+                break
             break
