cf327e4c39244ecd7ce13e0eaa855f5565dd6a1b
Gustavo Nam <gustavosmn@al.insper.edu.br>
2022-11-13 15:38:32 -0300

    substituição do for por while para concertar erros, adicionou manual, acição de cores

--- códigos.py
+++ códigos.py
@@ -1,4 +1,5 @@
 import funcoes
+from time import sleep
 
 pontos = 0
 niveis = []
@@ -13,16 +14,29 @@ lista_niveis = funcoes.transforma_base(funcoes.quest)
 for k, v in lista_niveis.items():
     if k not in niveis:
         niveis.append(k)
-jogador = str(input('Bem vindo ao jogo da EP2 de desoft!\nQual seu nome? '))
-print(f'Olá \033[0;35m{jogador}\033[0;m, vamos começar?\n')
+print('\033[0;31mOlá Bem vindo ao jogo da EP2 de desoft!\n\033[0m')
+jogador = str(input('\n\033[0;36mQual seu nome?\033[0m '))
+print(f'\033[0mOlá \033[0;35m{jogador}\033[0;m\n')
+sleep(1)
+print(f'responda as perguntas para receber pontos!')
+sleep(1)
+print('cada pergunta respondida aumenta sua pontuação')
+sleep(1)
+print('você tem direito a 3 DICAS e 2 PULOS')
+sleep(1)
+print('o jogo acaba se errar ou chegar aos 1000000 pontos')
+sleep(1)
+print('vamos começar?')
+sleep(1)
 while pontos < 1000000:
     if pontos == 0:
-        print(f'Vamos começar com as questões de nivel {niveis[0]}')
-    if 0 <= pontos < 30000:
-        print(f'PONTUAÇÃO ATUAL: {pontos}')
+        print(f'{"-"*45}\nVamos começar com as questões de nivel {niveis[0]}\n{"-"*45}\n')
+        sleep(2)
+    if 0 <= pontos < 5000:
+        print(f'PONTUAÇÃO ATUAL: \033[0;33m{pontos}\033[0m')
         quest1 = funcoes.sorteia_questao_inedida(lista_niveis, 'facil', lista1)
         prints = funcoes.questao_para_texto(quest1, cont)
-        while 0 <= pontos < 30000:
+        while 0 <= pontos < 5000:
 
             rus = str(input(f'''para dica digite "J" ({ajudas} restantes)
     para pular questão digite "P" ({pulos} restantes)
@@ -44,7 +58,7 @@ while pontos < 1000000:
                 if ajudas > 0:
                     ajudas -= 1
                     ajd = funcoes.gera_ajuda(quest1)
-                    print(ajd)
+                    print(f'\033[0;36m{ajd}\033[0Fm')
                 else:
                     print('sem ajudas restantes')
                     print(prints)
@@ -68,14 +82,15 @@ while pontos < 1000000:
     if key_word == 'babana':
         break
 
-    elif 30000 <= pontos < 100000:
-        if pontos == 30000:
-            print(f'Vamos continuar com as questões de nivel {niveis[1]}')
-        if 30000 <= pontos < 100000:
-            print(f'PONTUAÇÃO ATUAL: {pontos}')
+    elif 5000 <= pontos < 100000:
+        if pontos == 5000:
+            print(f'{"-"*47}\nVamos continuar com as questões de nivel {niveis[1]}\n{"-"*47}')
+            sleep(2)
+        if 5000 <= pontos < 100000:
+            print(f'PONTUAÇÃO ATUAL: \033[0;33m{pontos}\033[0m')
             quest1 = funcoes.sorteia_questao_inedida(lista_niveis, 'medio', lista1)
             prints = funcoes.questao_para_texto(quest1, cont)
-            while 30000 <= pontos < 100000:
+            while 5000 <= pontos < 100000:
 
                 rus = str(input(f'''para dica digite "J" ({ajudas} restantes)
         para pular questão digite "P" ({pulos} restantes)
@@ -97,7 +112,7 @@ while pontos < 1000000:
                     if ajudas > 0:
                         ajudas -= 1
                         ajd = funcoes.gera_ajuda(quest1)
-                        print(ajd)
+                        print(f'\033[0;36m{ajd}\033[0Fm')
                     else:
                         print('sem ajudas restantes')
                         print(prints)
@@ -122,9 +137,10 @@ while pontos < 1000000:
             break
     elif 100000 <= pontos < 1000000:
         if pontos == 100000:
-            print(f'Vamos continuar com as questões de nivel {niveis[2]}')
+            print(f'{"-"*49}\nVamos continuar com as questões de nivel {niveis[2]}\n{"-"*49}')
+            sleep(2)
         if 100000 <= pontos < 1000000:
-            print(f'PONTUAÇÃO ATUAL: {pontos}')
+            print(f'PONTUAÇÃO ATUAL: \033[0;33m{pontos}\033[0m')
             quest1 = funcoes.sorteia_questao_inedida(lista_niveis, 'dificil', lista1)
             prints = funcoes.questao_para_texto(quest1, cont)
             while 100000 <= pontos < 1000000:
@@ -149,7 +165,7 @@ while pontos < 1000000:
                     if ajudas > 0:
                         ajudas -= 1
                         ajd = funcoes.gera_ajuda(quest1)
-                        print(ajd)
+                        print(f'\033[0;36m{ajd}\033[0Fm')
                     else:
                         print('sem ajudas restantes')
                         print(prints)
